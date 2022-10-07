4eac25d20c2045fa03a6113e0d70f409c80eabfd
sunilkashya <101803053+sunilkashya@users.noreply.github.com>
2022-10-07 20:39:42 +0530

    Update execution.groovy

--- vars/execution.groovy
+++ vars/execution.groovy
@@ -1,5 +1,4 @@
 def call(Map config = [:]) {
   sh "sudo ${config.package} install ansible -y"
-  sh "cd ${config.path}"
-  sh "cat /var/lib/jenkins/workspace/job1/main/elasticsearch/inventory"
+  sh "ansible-playbook -i ${config.pat}/inventory ${config.pat}/${playybook_name}"
 }
