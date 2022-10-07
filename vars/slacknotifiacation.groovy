531c94043c4415da84adafe7a7d2d463eafe07ef
sunilkashya <101803053+sunilkashya@users.noreply.github.com>
2022-10-07 23:25:57 +0530

    Update slacknotifiacation.groovy

--- vars/slacknotifiacation.groovy
+++ vars/slacknotifiacation.groovy
@@ -1,3 +1,3 @@
 def call() {
-  slackSend channel: 'jenkinsassign', message: 'Your ${env.JOB_NAME} is suceesful.'
+  slackSend channel: 'jenkinsassign', message: "'Your ${env.JOB_NAME} is suceesful. Its build number and build url are  ${env.BUILD_NUMBER} and ${env.BUILD_URL}.'"
 }  
