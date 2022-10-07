9d9ac36827f83aac80fad6ed7ae0f13512a0258e
sunilkashya <101803053+sunilkashya@users.noreply.github.com>
2022-10-07 20:06:31 +0530

    Update execution.groovy

--- vars/execution.groovy
+++ vars/execution.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-  sh "${config.package} install ansible -y"
+  sh "sudo ${config.package} install ansible -y"
   sh "cd ${config.path}"
   sh "ansible-playbook -i inventory ${config.playbook_name} "
 }
